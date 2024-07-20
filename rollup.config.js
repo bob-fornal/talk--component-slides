5ba5d9cd51dfb359478f554a0248cc7f92c6d479
Bob Fornal <bob.fornal@leadingedje.com>
2024-07-19 20:18:18 -0400

    Fixing Dist Structure

--- rollup.config.js
+++ rollup.config.js
@@ -10,9 +10,9 @@ import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 
 export default {
-  input: 'my-element.js',
+  input: ['src/slide-cover/slide-cover.ts'],
   output: {
-    file: 'my-element.bundled.js',
+    dir: 'dist/',
     format: 'esm',
   },
   onwarn(warning) {
